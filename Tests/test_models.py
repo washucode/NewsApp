fc35d9d61f1fa6c0fede34ca591ff8668890b6ec
washucode <estherw8525@gmail.com>
2019-09-08 02:40:24 +0300

    add test to check instance variables

--- Tests/test_models.py
+++ Tests/test_models.py
@@ -1,5 +1,5 @@
 import unittest
-from app import models
+from app.models import  Sources,News_Articles
 
 class testSources(unittest.TestCase):
     '''
@@ -10,3 +10,15 @@ class testSources(unittest.TestCase):
         This will run before every other test
         '''
         self.new_source = Sources('ansa','ANSA.it','Agenzia ANSA: ultime notizie, foto, video e approfondimenti su...','http://www.ansa.it','general','it','it')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+     def test_checkinstancevariables(self):
+        self.assertEquals(self.new_source.id,'ansa')
+        self.assertEquals(self.new_source.name,'ANSA.it')
+        self.assertEquals(self.new_source.description,'Agenzia ANSA: ultime notizie, foto, video e approfondimenti su...')
+        self.assertEquals(self.new_source.url,'http://www.ansa.it')
+        self.assertEquals(self.new_source.category,'general')
+        self.assertEquals(self.new_source.country,'it')
+        self.assertEquals(self.new_source.language,'it')
