73ac19c9a69b78e63a6bf954d497cc2c548b4540
washucode <estherw8525@gmail.com>
2019-09-08 02:43:06 +0300

    add sources class and initiate its variables

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Sources:
+    '''
+    class to define Sources instances
+    '''
+
+    def __init__(self,id,name,description,url,category,country,language):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
