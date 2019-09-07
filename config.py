5e128042eea9181f5c9028c5cf36dd07919635d3
washucode <estherw8525@gmail.com>
2019-09-07 16:04:59 +0300

    add bootstrap and config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+
+
+class Config:
+
+    
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
