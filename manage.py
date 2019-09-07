9c68f2b1fab5999102ef0df388f9ffbd9a52fbf4
washucode <estherw8525@gmail.com>
2019-09-08 02:44:56 +0300

    update manage.py

--- manage.py
+++ manage.py
@@ -7,6 +7,13 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('Test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == "__main__":
     manager.run()
 
