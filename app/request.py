d59a4b81dae4a55aca05ee7861083288e7d169b7
washucode <estherw8525@gmail.com>
2019-09-08 22:56:41 +0300

    add request for getting specific articles

--- app/request.py
+++ app/request.py
@@ -1,9 +1,10 @@
 import requests
-from .models import Sources
+from .models import Sources, News_Article
 
 
 base_news_url = None
 base_articles_url = None
+api_key = None
   
 def configure_request(app):
 	global api_key,base_news_url,base_articles_url
@@ -17,7 +18,7 @@ def news(category):
     source_url = base_news_url.format(category,api_key)
     my_sources = requests.get(source_url).json()
     my_results = my_sources['sources']
-    print (my_results)
+    
   
     my_final_sources = []
 
@@ -33,6 +34,30 @@ def news(category):
 
         sources_object = Sources(id,name,description,url,category,country,language)
         my_final_sources.append(sources_object)
+
+
+    return my_final_sources
+
+def news_article(id):
+    source_url = base_articles_url.format(id,api_key)
+    my_article = requests.get(source_url).json()
+    my_results = my_article['articles']
+    
+  
+    my_final_article = []
+
+    
+    for article in my_results :
+        id = article['id']
+		author = article['author']
+		title = article['title']
+		description = article['description']
+		url = article['url']
+		urlToImage = article['urlToImage']
+		publishedAt = article['publishedAt']
+
+        articles_object = News_Article(id,author,title,description,url,urlToImage,publishedAt)
+        my_final_article.append(articless_object)
 
 
     return my_final_sources
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os
+
+
 class Config:
-    pass
+    SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWSARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
     
 class ProdConfig(Config):
