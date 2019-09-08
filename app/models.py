5efc2e69599221f72119a00d7eda09b5ff25355d
washucode <estherw8525@gmail.com>
2019-09-08 03:26:43 +0300

    add class News_Articles and a method to define instance variables

--- app/models.py
+++ app/models.py
@@ -11,3 +11,17 @@ class Sources:
         self.category = category
         self.country = country
         self.language = language
+
+class News_Articles:
+    '''
+    class to define Sources instances
+    '''
+
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
